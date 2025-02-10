0e297e2d36495c239bc9eb1b9a724b44dfb726d7
Deries Ballesteros <DBALL10305@gmail.com>
2025-02-10 22:44:27 +1100

    initialise variables in global scope

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log("hello world")
+// console.log("hello world")
+
+// INITIALISE variables in global 
+let humanScore = 0;
+let computerScore
+
+// Create new function getComputerChoice
+// GENERATE random number between 0-2
+// IF generated number == 0
+    // RETURN "Rock"
+// ELSE IF generated number == 1
+    // RETURN "Paper"
+// ELSE
+    // RETURN"Scissors"
+
+// Create new function getHumanChoice
+// INPUT user choice (rock, paper, scissors)
+// RETURN user choice
+
+// Create new function playRound
+// IF user choice == "rock"
+    // DISPLAY You chose rock, computer chose scissors, YOU WIN!
+// ELSE IF user choice == "paper"
+    // DISPLAY You chose paper, computer chose rock YOU WIN
+// ELSE IF user choice == "scissors"
+    // DISPLAY You chose scissors, computer chose paper YOU WIN
+// ELSE IF user choice == computer choice
+    // DISPLAY TIE
+// ELSE
+    // DISPLAY YOU LOSE
+
+// Create function playGame
+// CALL playRound function 5 times
+// DISPLAY final score and win
